163af0287b4568851f1956e00101db100fc68486
Sami <samimhd1207@gmail.com>
2026-04-01 17:52:24 +0200

    fond d ecran

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -27,29 +27,9 @@ export function Welcome() {
 }
 
 const resources = [
-    {
-    href: "https://mikrotik.com/support",
-    text: "Demnade d'assistance sur Mikrotik",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-blue-200 transition group-hover:stroke-white"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
-  },
   {
-    href: "https://www.enreach.com/fr/contactez-nous/support-client",
-    text: "Demande de routage sur Enreach",
+    href: "https://reactrouter.com/docs",
+    text: "React Router Docs",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -67,10 +47,9 @@ const resources = [
       </svg>
     ),
   },
- 
   {
     href: "https://rmx.as/discord",
-    text: "Demande de routage sur Webex",
+    text: "Join My application",
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
